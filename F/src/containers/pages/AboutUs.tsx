4bc71761e95d611101420ecdafbf84b8fd185acf
Khanh <nguyenkhanhan79@gmail.com>
2023-09-26 05:23:55 +0700

    thêm trang about us - chỉnh sửa lại home section với content phù hợp hơn

---
+++ F/src/containers/pages/AboutUs.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import "./Content.css";
+import "containers/pages/Content.css";
+import "font-awesome/css/font-awesome.css";
+import '@fortawesome/fontawesome-free/css/all.css';
+import Footer from 'components/Footer';
+import Homesection1 from 'components/Homesection1';
+import Cardsession from 'containers/pages/CardSession';
+
+const AboutUs = () => {
+    return (
+      <>
+  
+        
+      </>
+    );
+  };
+  
+  
+  export default AboutUs;
