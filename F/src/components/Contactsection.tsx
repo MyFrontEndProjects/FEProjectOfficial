cf9f638e85e7fad55c7d636f55179dc13a777124
Khanh <nguyenkhanhan79@gmail.com>
2023-09-26 19:00:58 +0700

    them iframe

--- F/src/components/Contactsection.tsx
+++ F/src/components/Contactsection.tsx
@@ -115,9 +115,26 @@ function Contactsection() {
                 </p>
               </div>
             </div>
+
+            <div className="col-lg-12 d-flex justify-content-center">
+              {/* iframe chứa bản đồ Google */}
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2090.7718689341546!2d106.63011618799926!3d10.852837196383904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175295349d0d5a3%3A0xced384ab00155ada!2sT%C3%B2a%20nh%C3%A0%20Saigon%20ICT-%20Saigontel!5e0!3m2!1svi!2s!4v1695729232376!5m2!1svi!2s"
+                width={600}
+                height={450}
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title="Bản đồ"
+              />
+            </div>
+
           </div>
         </div>
       </section>
+
+
 
 
     </>
