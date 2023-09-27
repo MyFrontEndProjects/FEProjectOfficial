aa0cac67d2e41e0cf21c5a9a4e4ad8f0b3f5f7c7
Khanh <nguyenkhanhan79@gmail.com>
2023-09-28 04:42:36 +0700

    chỉnh sửa trang navbar

--- F/src/containers/home/Header.tsx
+++ F/src/containers/home/Header.tsx
@@ -4,15 +4,7 @@ import Navbar from 'components/NavBar'
 function Header() {
     return (
         <>
-            {/* <Navbar bg='dark' variant='dark'>
-                <Navbar.Brand href ='#home'>Logo</Navbar.Brand>
-                <Nav className='mr-auto'>
-                    <Link to='/add'>Add</Link>
-                    <Link to='/update'>update</Link>
-                    <Link to='/login'>login</Link>
-                    <Link to='/register'>Register</Link>
-                </Nav>
-            </Navbar> */}
+          
             <Navbar/>
         </>
     )
