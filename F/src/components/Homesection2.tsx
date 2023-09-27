d1da5593b76de39f92287d8b493731f7f85172d0
Khanh <nguyenkhanhan79@gmail.com>
2023-09-28 03:06:50 +0700

    hoàn thiện trang home

--- F/src/components/Homesection2.tsx
+++ F/src/components/Homesection2.tsx
@@ -27,8 +27,12 @@ const Homesection2 = () => {
                     <div className="col-12 col-md-6 col-lg-4">
                         <div className="card mb-4 border-0 bg-dark">
                             {/* Hình ảnh sản phẩm */}
-                            <img src="./img/store14.webp" alt="" className="card-img-top" />
-
+                            <div
+                                className="image-container"
+                                style={{ height: 350, overflow: "hidden", marginTop: 10 }}
+                            >
+                                <img src="./img/store14.webp" alt="" className="card-img-top" />
+                            </div>
                             {/* Nội dung card */}
 
                             <div className="card-body">
@@ -58,7 +62,12 @@ const Homesection2 = () => {
                     <div className="col-12 col-md-6 col-lg-4">
                         <div className="card mb-4 border-0 bg-dark">
                             {/* Hình ảnh sản phẩm */}
-                            <img src="./img/store13.webp" alt="" className="card-img-top" />
+                            <div
+                                className="image-container"
+                                style={{ height: 350, overflow: "hidden", marginTop: 10 }}
+                            >
+                                <img src="./img/store13.webp" alt="" className="card-img-top" />
+                            </div>
 
                             {/* Nội dung card */}
                             <div className="card-body">
@@ -88,7 +97,11 @@ const Homesection2 = () => {
                     <div className="col-12 col-md-6 col-lg-4">
                         <div className="card mb-4 border-0 bg-dark">
                             {/* Hình ảnh sản phẩm */}
-                            <img src="./img/store12.webp" alt="" className="card-img-top" />
+                            <div
+                                className="image-container"
+                                style={{ height: 350, overflow: "hidden", marginTop: 10 }}
+                            >
+                                <img src="./img/store12.webp" alt="" className="card-img-top" /></div>
 
                             {/* Nội dung card */}
 
@@ -123,12 +136,12 @@ const Homesection2 = () => {
 
 
 
-                
-                    <div className=" d-flex justify-content-center  py-5 m-5" style={{ fontFamily: 'cursive' }}>
-                        <h5 className="text-white">CÁC SẢN PHẨM KHÁC  <span className="text-warning"> <i className="fa-solid fa-arrow-trend-up" />
-                        </span></h5>
-                    </div>
-              
+
+                <div className=" d-flex justify-content-center  py-5 m-5" style={{ fontFamily: 'cursive' }}>
+                    <h5 className="text-white">CÁC SẢN PHẨM KHÁC  <span className="text-warning"> <i className="fa-solid fa-arrow-trend-up" />
+                    </span></h5>
+                </div>
+
 
                 <div className="row d-flex justify-content-center">
                     <div className="col-12 col-md-8 col-lg-10 ">
@@ -150,20 +163,24 @@ const Homesection2 = () => {
                     <div className="col-12 col-md-6 col-lg-4">
                         <div className="card mb-4 border-0 bg-dark">
                             {/* Hình ảnh sản phẩm */}
-                            <img src="./img/store14.webp" alt="" className="card-img-top" />
-
+                            <div
+                                className="image-container"
+                                style={{ height: 350, overflow: "hidden", marginTop: 10 }}
+                            >
+                                <img src="./img/store15.webp" alt="" className="card-img-top" />
+                            </div>
                             {/* Nội dung card */}
 
                             <div className="card-body">
-                                <h3 className="card-title text-center text-white-50">Nitro 16 AMD</h3>
+                                <h3 className="card-title text-center text-white-50">PREDATOR HELIOS NEO 16</h3>
                                 <p className="card-text text-white mx-5">
                                     AMD Ryzen™ 5 7640HS processor
                                     <br />
-                                    NVIDIA® GeForce RTX™ 4050
+                                    Intel® Core™ i7-13700HX
                                     <br />
                                     8 GB, DDR5 SDRAM
                                     <br />
-                                    512 GB SSD
+                                    1 TB SSD
                                 </p>
                             </div>
 
@@ -181,50 +198,58 @@ const Homesection2 = () => {
                     <div className="col-12 col-md-6 col-lg-4">
                         <div className="card mb-4 border-0 bg-dark">
                             {/* Hình ảnh sản phẩm */}
-                            <img src="./img/store13.webp" alt="" className="card-img-top" />
+                            <div
+                                className="image-container"
+                                style={{ height: 350, overflow: "hidden", marginTop: 10 }}
+                            >
+                                <img src="./img/store15.webp" alt="" className="card-img-top" /></div>
 
                             {/* Nội dung card */}
                             <div className="card-body">
-                                <h3 className="card-title text-center text-white-50">Nitro 16 Intel</h3>
+                                <h3 className="card-title text-center text-white-50">PREDATOR HELIOS NEO 16</h3>
                                 <p className="card-text text-white mx-5">
-                                    Intel® Core™ i5-13500H
-                                    <br />
-                                    NVIDIA® GeForce RTX™ 4050
-                                    <br />
-                                    8 GB, DDR5 SDRAM
-                                    <br />
-                                    512 GB SSD
-                                </p>
-                            </div>
-
-                        </div>
-
-                        <div className="d-flex justify-content-center">
-                            <button className="btn btn-warning text-black d-flex align-items-center">
-                                Xem thêm
-                                <i className="fa-solid fa-cart-plus ms-2" />
-                            </button>
-                        </div>
-
-                    </div>
-
-                    <div className="col-12 col-md-6 col-lg-4">
-                        <div className="card mb-4 border-0 bg-dark">
-                            {/* Hình ảnh sản phẩm */}
-                            <img src="./img/store12.webp" alt="" className="card-img-top" />
-
-                            {/* Nội dung card */}
-
-                            <div className="card-body">
-                                <h3 className="card-title text-center text-white-50">Acer Nitro 17 AMD</h3>
-                                <p className="card-text text-white mx-5">
-                                    AMD Ryzen™ 7 7840HS
+                                    Intel® Core™ i7-13700HX
                                     <br />
                                     NVIDIA® GeForce RTX™ 4050
                                     <br />
                                     16 GB, DDR5 SDRAM
                                     <br />
-                                    1 TB SSD
+                                    512 GB SSD
+                                </p>
+                            </div>
+
+                        </div>
+
+                        <div className="d-flex justify-content-center">
+                            <button className="btn btn-warning text-black d-flex align-items-center">
+                                Xem thêm
+                                <i className="fa-solid fa-cart-plus ms-2" />
+                            </button>
+                        </div>
+
+                    </div>
+
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <div className="card mb-4 border-0 bg-dark">
+                            {/* Hình ảnh sản phẩm */}
+                            <div
+                                className="image-container"
+                                style={{ height: 350, overflow: "hidden", marginTop: 10 }}
+                            >
+                                <img src="./img/store15.webp" alt="" className="card-img-top" /></div>
+
+                            {/* Nội dung card */}
+
+                            <div className="card-body">
+                                <h3 className="card-title text-center text-white-50">PREDATOR TRITON 17 X</h3>
+                                <p className="card-text text-white mx-5">
+                                    Intel® Core™ i9-13900HX
+                                    <br />
+                                    NVIDIA® GeForce RTX™ 4090
+                                    <br />
+                                    64 GB, DDR5 SDRAM
+                                    <br />
+                                    2 TB SSD
                                 </p>
                             </div>
 
@@ -245,7 +270,12 @@ const Homesection2 = () => {
             </section>
 
 
-
+            <style
+                dangerouslySetInnerHTML={{
+                    __html:
+                        "\n    .card-footer {\n        width: 100%;\n        display: flex;\n        justify-content: space-between;\n        align-items: center;\n        padding-top: 10px;\n        border-top: 1px solid #ddd;\n    }\n\n    .text-title>a {\n        text-decoration: none;\n    }\n\n    .text-title {\n        font-weight: 900;\n        font-size: 1.2em;\n        line-height: 1.5;\n    }\n\n\n    /*Button*/\n    .card-button {\n        border: 1px solid #252525;\n        display: flex;\n        padding: .3em;\n        cursor: pointer;\n        border-radius: 50px;\n        transition: .3s ease-in-out;\n    }\n\n    /*Hover*/\n    /* Khi ảnh nằm trong một container */\n    .image-container {\n        position: relative;\n        /* Để xác định vị trí tương đối */\n        overflow: hidden;\n        /* Ẩn phần ảnh vượt ra khỏi container */\n    }\n\n    /* Ảnh ban đầu */\n    .image-container img {\n        width: 100%;\n        /* Đặt chiều rộng ban đầu */\n        height: auto;\n        /* Tự động tính chiều cao */\n        transition: transform 0.3s;\n        /* Hiệu ứng chuyển đổi */\n    }\n\n    /* Ảnh khi hover */\n    .image-container:hover img {\n        transform: scale(1.1);\n        /* Phóng to ảnh khi hover */\n    }\n\n    .card-button:hover {\n        border: 1px solid #ffcaa6;\n        background-color: #ffcaa6;\n    }\n",
+                }}
+            />
 
 
 
